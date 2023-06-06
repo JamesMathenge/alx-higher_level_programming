7f171da7028fdb7364822b1f0051c7ee11d5c079
JamesMathenge <mathengejames64@gmail.com>
2023-06-06 18:40:40 +0000

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 
 for y in range(10):
     for z in range(y + 1, 10):
-        print("{:d}{:d}".format(y, z), end=", ")
-print("\n")
+        print("{:d}{:d}".format(y, z), end=", " if y != 8 or z != 9 else "")
+print()
