eee7b8cd12718d60f662258fc559df8e555b5538
JamesMathenge <mathengejames64@gmail.com>
2023-06-27 10:49:36 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+
+    A class that defines a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
