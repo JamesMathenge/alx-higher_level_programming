08120b91968a786dfed3a697d1d157e8ae4cc53a
JamesMathenge <mathengejames64@gmail.com>
2023-07-10 20:28:28 +0000

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
 Defines a Rectangle class that inherits from BaseGeometry.
-
 """
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
@@ -20,10 +19,10 @@ class Rectangle(BaseGeometry):
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
     def __str__(self):
         """
@@ -33,3 +32,20 @@ class Rectangle(BaseGeometry):
             str: The string representation of the Rectangle.
         """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def integer_validator(self, name, value):
+        """
+        Validates that a value is a positive integer.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not positive.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
