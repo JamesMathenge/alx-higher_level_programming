58ace16a26db2e7f2c3632c76e035b34e11703b2
JamesMathenge <mathengejames64@gmail.com>
2023-07-10 20:49:26 +0000

    task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,25 +1,60 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle that inherits from BaseGeometry."""
+"""
+Defines a Rectangle class that inherits from BaseGeometry.
+"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """this class represents a rectangle using BaseGeometry"""
+    """
+    Represents a rectangle.
+    """
 
     def __init__(self, width, height):
-        """Intialize a new rectangle"""
+        """
+        Initializes a Rectangle instance.
 
-        super().integer_validator("width", width)
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
         self.__width = width
-        super().integer_validator("height", height)
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
-        def area(self):
-            """returns the area of the rectangle"""
-            return self.__width * self.__height
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle.
 
-        def __str__(self):
-            """returns the print() and str() representation of a Rectangle"""
-            string = "[" + str(self.__class__.__name__) + "] "
-            string += str(self.__width) + "/" + str(self.__height)
-            return string
+        Returns:
+            str: The string representation of the Rectangle.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """
+        Calculates the area of the Rectangle.
+
+        Returns:
+            int: The area of the Rectangle.
+        """
+        return self.__width * self.__height
+
+    def integer_validator(self, name, value):
+        """
+        Validates that a value is a positive integer.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not positive.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
