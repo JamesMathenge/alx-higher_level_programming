f0f3ebbf2a105752a3f56e9c173db460425c833c
JamesMathenge <mathengejames64@gmail.com>
2023-07-12 16:08:45 +0000

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a class Student"""
-
-
 class Student:
-    """Represent a student."""
-
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
-                    """
+        """Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            """Gets a dictionary representation of the Student"""
-            return self.__dict__
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: dictionary contains first_name, last_name, and age attr.
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
