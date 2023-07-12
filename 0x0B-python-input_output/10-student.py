0ff3b6488a27925f1d5482685992060435d70ab4
JamesMathenge <mathengejames64@gmail.com>
2023-07-12 16:25:41 +0000

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,24 +3,42 @@
 
 
 class Student:
-    """Represent a student."""
+    """
+    A class representing a Student.
+
+    Attributes:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+    """
 
     def __init__(self, first_name, last_name, age):
         """
-        Initializes a new Student
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self, attrs=None):
-            """
-            Get a dictionary representation of the Student
-            If attrs is a list of strings, represents only those attributes
-            included in the list
-            """
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
 
-            if (type(attrs) == list and
-                    all(type(ele) == str for ele in attrs)):
-                return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        Args:
+            attrs (list):list of attribute names retrieved (default: None).
+
+        Returns:
+            dict: dictionary containing specified attribute names
+            and their values if attrs is None,
+            returns a dictionary containing all attributes and their values.
+        """
+        if attrs is None:
             return self.__dict__
+        else:
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
