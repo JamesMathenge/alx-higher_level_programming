a726c1a1406de1fea47e7165323f26599adc40cf
JamesMathenge <mathengejames64@gmail.com>
2023-07-12 16:58:47 +0000

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,15 +3,46 @@
 
 
 import sys
+from os.path import exists
+from json import dump, load
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-        except FileNotFoundError:
-            items = []
-            items.extend(sys.argv[1:])
-            save_to_json_file(items, "add_item.json")
+def save_to_json_file(my_obj, filename):
+    """
+    Save an object to a JSON file.
+
+    Args:
+        my_obj: The object to be saved.
+        filename (str): The name of the file.
+
+    Returns:
+        None
+    """
+    with open(filename, 'w') as file:
+        dump(my_obj, file)
+
+
+def load_from_json_file(filename):
+    """
+    Load an object from a JSON file.
+
+    Args:
+        filename (str): The name of the file.
+
+    Returns:
+        The loaded object.
+    """
+    with open(filename, 'r') as file:
+        return load(file)
+
+
+if __name__ == '__main__':
+    filename = 'add_item.json'
+
+    if exists(filename):
+        my_list = load_from_json_file(filename)
+    else:
+        my_list = []
+
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, filename)
