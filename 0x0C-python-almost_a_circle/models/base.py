6de3cf0bc44f3ecc8698a61ff5b8bcecc78bc8c9
JamesMathenge <mathengejames64@gmail.com>
2023-07-16 17:50:00 +0000

    Update the class Base by adding the static method def to_json_string(list_dictionaries): that returns the JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """This module contains a class to serve as base for other classes"""
 
 import unittest
+import json
 
 
 class Base:
@@ -16,3 +17,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of a list of dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
