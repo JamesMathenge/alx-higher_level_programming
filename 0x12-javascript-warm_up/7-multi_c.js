8630a6c8fd1f6c7f113c5be01a12a4b9b903f04c
JamesMathenge <mathengejames64@gmail.com>
2023-09-11 22:19:22 +0000

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,10 +5,10 @@ const Arg = process.argv[2];
 
 const x = parseInt(Arg);
 
-if (!isNaN(x)){
-	for (let z = 0; z < x; z++){
-		console.log('C is fun');
-	}
-} else{
-	console.log('Missing number of occurrences');
+if (!isNaN(x)) {
+  for (let z = 0; z < x; z++) {
+    console.log('C is fun');
+}
+} else {
+  console.log('Missing number of occurrences');
 }
