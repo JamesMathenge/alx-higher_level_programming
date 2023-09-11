2eea75ec9313007dd2e96cf8d5ffd09d66dab510
JamesMathenge <mathengejames64@gmail.com>
2023-09-11 21:52:00 +0000

    task 0,1,2,3

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arguments_Count = process.argv.length - 2;
+
+if (arguments_Count === 0) {
+	console.log('No argument');
+} else if (arguments_Count === 1) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+const Args = process.argv;
+if (Args[2]) {
+	console.log(Args[2]);
+} else {
+	console.log('No argument');
+}
