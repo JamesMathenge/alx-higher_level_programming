781b11576e8a3a014643a6f658b1c766871688a8
JamesMathenge <mathengejames64@gmail.com>
2023-09-11 22:30:19 +0000

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ const process = require('process');
 
 const Args = process.argv;
 if (Args[2]) {
-	console.log(Args[2]);
+  console.log(Args[2]);
 } else {
-	console.log('No argument');
+  console.log('No argument');
 }
