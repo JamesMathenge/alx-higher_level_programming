9123037772fb1c062441763c0e0615024941054b
JamesMathenge <mathengejames64@gmail.com>
2023-09-11 22:07:05 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const arguments_Count = process.argv.length - 2;
 
 if (arguments_Count === 0) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (arguments_Count === 1) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
