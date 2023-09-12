679a471e291075ff8e0def4b74f20018c3697870
JamesMathenge <mathengejames64@gmail.com>
2023-09-12 20:39:53 +0000

    task 4-5

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -6,24 +6,24 @@ module.exports = class Rectangle {
       this.width = w;
       this.height = h;
     }
-}
-
-print () {
-  for (let i = 0; i < this.height; i++) {
-    let row = '';
-    for (let j = 0; j < this.width; j++) {
-      row += 'X';
-    }
-    console.log(row);
   }
-}
 
-rotate () {
-  [this.width, this.height] = [this.height, this.width];
-}
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
 
-double () {
-  this.width *= 2;
-  this.height *= 2;
-}
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 };
