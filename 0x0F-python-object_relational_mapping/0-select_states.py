145886b711a3fdd1f042f168b6ecb54a490cc1a8
JamesMathenge <mathengejames64@gmail.com>
2023-09-17 23:55:11 +0000

    task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module that connects a python script to a database
+"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
+    my_db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],
+                            db=argv[3], port=3306)
+
+    my_cursor = my_db.cursor()
+
+    my_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
+    my_data = my_cursor.fetchall()
+
+    for row in my_data:
+        print(row)
+
+        my_cursor.close()
+
+        my_db.close()
