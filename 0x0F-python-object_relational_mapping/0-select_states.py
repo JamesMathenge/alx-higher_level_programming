ece43caf24c26dd73e4362f7fe2842b890cfde61
JamesMathenge <mathengejames64@gmail.com>
2023-09-21 19:14:53 +0000

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,37 +9,37 @@ if __name__ == "__main__":
             sys.argv[0]))
         sys.exit(1)
 
-        mysql_username = sys.argv[1]
-        mysql_password = sys.argv[2]
-        database_name = sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-        try:
-            # Connect to the MySQL server
-            db = MySQLdb.connect(
-                host="localhost",
-                user=mysql_username,
-                passwd=mysql_password,
-                db=database_name,
-                port=3306
-            )
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name,
+            port=3306
+        )
 
-            # Create a cursor object to execute SQL queries
-            cursor = db.cursor()
+        # Create a cursor object to execute SQL queries
+        cursor = db.cursor()
 
-            # Execute the SQL query to select all states and order by id
-            cursor.execute("SELECT * FROM states ORDER BY id")
+        # Execute the SQL query to select all states and order by id
+        cursor.execute("SELECT * FROM states ORDER BY id")
 
-            # Fetch all the rows
-            rows = cursor.fetchall()
+        # Fetch all the rows
+        rows = cursor.fetchall()
 
-            # Print the results
-            for row in rows:
-                print(row)
+        # Print the results
+        for row in rows:
+            print(row)
 
-                except MySQLdb.Error as e:
-                    print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-                    sys.exit(1)
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
 
-                    finally:
-                        # Close the database connection
-                        db.close()
+    finally:
+        # Close the database connection
+        db.close()
