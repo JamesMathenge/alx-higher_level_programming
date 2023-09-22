6c566e9a1a21b17521f8e3105636f9d4afd45cbd
JamesMathenge <mathengejames64@gmail.com>
2023-09-22 23:20:06 +0000

    task 100

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
 
 """
-Module that connects python script to a database
+Module that connects to the database
 """
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from relationship_city import Base, City
-from relationship_state import State
 
-if __name__ == "__main__":
 
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True
-    )
 
-    Base.metadata.create_all(engine)
-    my_session_maker = sessionmaker(bind=engine)
-    my_session = my_session_maker()
 
-    my_session.add(City(name="San Francisco", state=State(name="California")))
-    my_session.commit()
+class State(Base):
+    """defining the state class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
 
-    my_session.close()
+    cities = relationship("City", backref="state", cascade="all, delete")
