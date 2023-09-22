fd728b01cf21a2cd992e930c27ee15343b3d6149
JamesMathenge <mathengejames64@gmail.com>
2023-09-22 21:26:25 +0000

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
+import MySQLdb
+import sys
+
 """
 Module that connects a python script to a database
 """
 
 if __name__ == "__main__":
+    # Check for the correct number of arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    import MySQLdb
-    from sys import argv
+    # Retrieve the command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    # Connect database using command-line arguments
-    my_db = MySQLdb.connect(
-        host='localhost',
-        user=argv[1],
-        password=argv[2],
-        db=argv[3],
-        port=3306
-    )
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=username, passwd=password, db=database)
 
-    # Create cursor obj to interact with database
-    my_cursor = my_db.cursor()
+        # Create a cursor object
+        cursor = db.cursor()
 
-    # Execute a SELECT query to fetch data
-    my_cursor.execute(
-        """SELECT cities.id, cities.name, states.name
-        FROM cities
-        JOIN states
-        ON states.id = cities.state_id
-        ORDER BY cities.id"""
-    )
+        # Execute the SQL query to fetch cities
+        cursor.execute("SELECT cities.id, cities.name, states.name FROM cities
+                       JOIN states ON cities.state_id=states.id
+                       ORDER BY cities.id ASC")
 
-    # fetch all the data returned by the query
-    my_data = my_cursor.fetchall()
+        # Fetch and print the results
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
 
-    # Iterate through the fetched data and print each row
-    for row in my_data:
-        print(row)
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
 
-        # Close all cursors
-        my_cursor.close()
-
-        # Close all databases
-        my_db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
